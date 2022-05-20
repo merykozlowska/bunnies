3ce275784f0ce9e1176ddc64fe61b8fc0d541f3d
adrienbaron <adrien@abaron.net>
2022-05-21 00:16:28 +0100

    improve button

--- app/views/home/components/button.tsx
+++ app/views/home/components/button.tsx
@@ -8,6 +8,12 @@ export type ButtonColour = "white" | "brown";
 
 export const Button: React.FC<
   JSX.IntrinsicElements["button"] & { buttonColor?: ButtonColour }
-> = ({ buttonColor = "white", ...props }) => (
-  <button {...props} className="customButton" data-colour={buttonColor} />
+> = ({ buttonColor = "white", children, ...props }) => (
+  <button {...props} className="customButton" data-colour={buttonColor}>
+    <span className="customButton__borderLeft" />
+    <span className="customButton__borderRight" />
+    <span className="customButton__borderTop" />
+    <span className="customButton__borderBottom" />
+    {children}
+  </button>
 );
