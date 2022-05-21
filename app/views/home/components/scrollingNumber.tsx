014120290c42b3ba4e0475a1678400f84a24267f
adrienbaron <adrien@abaron.net>
2022-05-21 15:06:55 +0100

    improve variable naming in scrollingNumber

--- app/views/home/components/scrollingNumber.tsx
+++ app/views/home/components/scrollingNumber.tsx
@@ -8,26 +8,28 @@ export const ScrollingNumber: React.FC<Props> = ({ value }) => {
   const [displayedValue, setDisplayedValue] = useState(value);
 
   useEffect(() => {
-    const duration = 150;
-    const diff = (value - displayedValue) / duration;
+    const totalDurationInMs = 150;
     const startValue = displayedValue;
+    const valueStepPerMs = (value - displayedValue) / totalDurationInMs;
 
-    let start = 0;
+    let startTimeInMs = 0;
     let updateAnimationFrame: number;
     const requestAnimation = () =>
-      window.requestAnimationFrame((dt) => {
-        if (start === 0) {
-          start = dt;
+      window.requestAnimationFrame((currentTimeInMs) => {
+        if (startTimeInMs === 0) {
+          startTimeInMs = currentTimeInMs;
         }
 
-        const timeSpent = dt - start;
-        if (timeSpent >= duration) {
+        const timePastInMs = currentTimeInMs - startTimeInMs;
+        if (timePastInMs >= totalDurationInMs) {
           setDisplayedValue(value);
           window.cancelAnimationFrame(updateAnimationFrame);
           return;
         }
 
-        setDisplayedValue(Math.floor(startValue + timeSpent * diff));
+        setDisplayedValue(
+          Math.floor(startValue + timePastInMs * valueStepPerMs)
+        );
         updateAnimationFrame = requestAnimation();
       });
 
