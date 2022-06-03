d7f1a95c27380f6cf59088f121c4eff32172601c
Maria Kozlowska <mery.kozlowska@gmail.com>
2022-06-03 11:25:16 +0100

    add api route to proxy to DO

---
+++ app/routes/api/game/$gameId/$.ts
@@ -0,0 +1,26 @@
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import type { Params } from "react-router";
+
+interface Context {
+  GAME: DurableObjectNamespace;
+}
+
+export const loader: LoaderFunction = async ({
+  context,
+  params,
+  request,
+}: {
+  context: Context;
+  params: Params;
+  request: Request;
+}) => {
+  if (params.gameId !== "global") {
+    return new Response("game id not allowed", { status: 400 });
+  }
+  const id = context.GAME.idFromName("global");
+  const gameObject = context.GAME.get(id);
+
+  const newUrl = new URL(request.url);
+  newUrl.pathname = params["*"] || "/";
+  return gameObject.fetch(newUrl.toString(), request);
+};
